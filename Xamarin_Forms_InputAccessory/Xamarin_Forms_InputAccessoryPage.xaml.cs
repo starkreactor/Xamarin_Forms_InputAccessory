048a330b7db40bc5f784308dfe61a4f7728b7aff
Michael Stark <mws580@hotmail.com>
2016-06-18 10:30:20 -0400

    no message

---
+++ Xamarin_Forms_InputAccessory/Xamarin_Forms_InputAccessoryPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin_Forms_InputAccessory
+{
+	public partial class Xamarin_Forms_InputAccessoryPage : ContentPage
+	{
+		public Xamarin_Forms_InputAccessoryPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
