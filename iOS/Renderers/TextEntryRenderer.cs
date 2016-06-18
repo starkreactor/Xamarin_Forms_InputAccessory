874212cbdefaf3e700ec8999fbb37c3610c8e3bd
Michael Stark <mws580@hotmail.com>
2016-06-18 11:25:59 -0400

    renderers added

---
+++ iOS/Renderers/TextEntryRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Xamarin_Forms_InputAccessory.iOS
+{
+	public class TextEntryRenderer : ContentPage
+	{
+		public TextEntryRenderer()
+		{
+			Content = new StackLayout
+			{
+				Children = {
+					new Label { Text = "Hello ContentPage" }
+				}
+			};
+		}
+	}
+}
+
+
